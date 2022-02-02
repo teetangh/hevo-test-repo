3668cc5bc0a4c3c5cc5a3305caf6f797dec99665
Kaustav Ghosh <teetangh@gmail.com>
2022-02-03 04:22:14 +0530

    added dropwizard CRUD project

---
+++ dropwizard-project/src/main/java/com/mycompany/api/Event.java
@@ -0,0 +1,12 @@
+package com.mycompany.api;
+
+public class Event {
+    private long id;
+    private String name;
+    private String description;
+    private String location;
+    private Date date;
+
+    //getters and setters omitted
+
+}
