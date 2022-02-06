cc6359491d3a560d7970a1749427c023ef251ecd
Kaustav Ghosh <teetangh@gmail.com>
2022-02-06 21:14:50 +0530

    added POST UPDATE and DELETE. working on authentication and authorization

--- dropwizard-project/src/main/java/com/mycompany/EventsApplication.java
+++ dropwizard-project/src/main/java/com/mycompany/EventsApplication.java
@@ -3,11 +3,19 @@ package com.mycompany;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
+import com.mycompany.auth.ExampleAuthenticator;
+import com.mycompany.auth.ExampleAuthorizer;
 import com.mycompany.core.DummyEventRepository;
 import com.mycompany.core.EventRepository;
 import com.mycompany.resources.EventResource;
 
+import org.eclipse.jetty.server.Authentication.User;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+
 import io.dropwizard.Application;
+import io.dropwizard.auth.AuthDynamicFeature;
+import io.dropwizard.auth.AuthValueFactoryProvider;
+import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
@@ -35,6 +43,17 @@ public class EventsApplication extends Application<EventsConfiguration> {
         EventRepository repository = new DummyEventRepository();
         EventResource eventResource = new EventResource(repository);
         environment.jersey().register(eventResource);
+
+        // Basic Authentication
+        environment.jersey().register(new AuthDynamicFeature(
+                new BasicCredentialAuthFilter.Builder<User>()
+                        .setAuthenticator(new ExampleAuthenticator())
+                        .setAuthorizer(new ExampleAuthorizer())
+                        .setRealm("SUPER SECRET STUFF")
+                        .buildAuthFilter()));
+        environment.jersey().register(RolesAllowedDynamicFeature.class);
+        // If you want to use @Auth to inject a custom Principal type into your resource
+        environment.jersey().register(new AuthValueFactoryProvider.Binder<>(User.class));
     }
 
 }
